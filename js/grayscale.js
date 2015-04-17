f1a4a171e119f9e88532c13852eee485aa5571e8
Gabriel Tan <tanjunxian@gmail.com>
2015-04-16 23:12:21 -0700

    update buttons

--- js/grayscale.js
+++ js/grayscale.js
@@ -21,7 +21,7 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top - 50
         }, 1000, 'easeInOutExpo');
         event.preventDefault();
     });

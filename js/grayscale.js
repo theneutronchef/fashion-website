f7ac8c090a6b4361af079ba67bcb22fe0535101d
Gabriel Tan <tanjunxian@gmail.com>
2015-04-16 22:18:02 -0700

    added peach background

--- js/grayscale.js
+++ js/grayscale.js
@@ -5,12 +5,15 @@
  */
 
 // jQuery to collapse the navbar on scroll
+
+
+
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
+    if ($(".navbar").offset().top > 100) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
+    }    
 });
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
@@ -19,7 +22,7 @@ $(function() {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        }, 1000, 'easeInOutExpo');
         event.preventDefault();
     });
 });
